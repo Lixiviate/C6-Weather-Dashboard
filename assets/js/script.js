500ca0cb2642f58813aef2e6f92b24330942c360
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-06-16 01:30:39 -0600

    Remove buttons, attempt to get search history working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const apiKey = "dc7f919c574b660b6b15490d06db38b6";
 const cityName = "Denver";
+let searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
 document
   .querySelector("#searchForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    const cityInput = document.querySelector("#cityInput").value;
+    const cityInput = document.querySelector("#cityInput").value.trim();
     getWeather(cityInput);
     console.log(cityInput);
   });
@@ -23,3 +24,18 @@ function getWeather(cityInput) {
       console.log("Error fetching data: ", error);
     });
 }
+
+function recentSearchHistory(city) {
+  searchHistory = searchHistory.filter(function (newCity) {
+    return newCity !== city;
+  });
+  searchHistory.unshift(city);
+  searchHistory = searchHistory.slice(0, 7);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+}
+
+function renderSearchHistory() {
+  const recentSearch = document.querySelector("#recentSearch");
+}
+
+renderSearchHistory();
