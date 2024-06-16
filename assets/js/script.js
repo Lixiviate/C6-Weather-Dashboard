1f16c9f75cbd3434de2e2654673775346a34d1b6
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-06-16 00:52:04 -0600

    Add JS to pull in city search and ping openweather api

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const apiKey = "dc7f919c574b660b6b15490d06db38b6";
+const cityName = "Denver";
+
+document
+  .querySelector("#searchForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const cityInput = document.querySelector("#cityInput").value;
+    getWeather(cityInput);
+    console.log(cityInput);
+  });
+
+function getWeather(cityInput) {
+  const queryURL = `api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`;
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
